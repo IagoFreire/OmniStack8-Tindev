5be46320d2ce5d2ea53a8e96f45382460da0ae1e
Iago FREIRE <iago.freire@ssa.corp>
2019-10-09 12:15:03 -0300

    Add ExpoMobile

---
+++ mobileExpo/src/pages/Login.js
@@ -0,0 +1,85 @@
+import React, {useEffect, useState} from 'react';
+import {Image, AsyncStorage, StyleSheet, Text, TextInput, TouchableOpacity,Platform, KeyboardAvoidingView} from 'react-native';
+
+import api from '../services/api'
+
+import logo from '../assets/logo.png';
+
+export default function Login({navigation}) {
+    const [user, setUser] = useState('');
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if (user) {
+                navigation.navigate('Main', {user})
+            }
+        })
+    }, []);
+
+    async function handleLogin() {
+        const response = await api.post('/devs', {username: user});
+
+        const {_id} = response.data;
+
+        await AsyncStorage.setItem('user', _id);
+
+        navigation.navigate('Main', {user: _id});
+    }
+
+    return (
+        <KeyboardAvoidingView enable={Platform.OS === 'ios'} behavior="padding" style={style.container}>
+            <Image source={logo}/>
+
+            <TextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                placeholder="Digite seu usuário no Github"
+                placeholderTextColor="#999"
+                style={style.input}
+                value={user}
+                onChangeText={setUser}
+            />
+
+            <TouchableOpacity onPress={handleLogin} style={style.button}>
+                <Text style={style.buttonText}>Enviar</Text>
+            </TouchableOpacity>
+        </KeyboardAvoidingView>
+    );
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#f5f5f5',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30
+    },
+
+    input: {
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#FFF',
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 4,
+        marginTop: 20,
+        paddingHorizontal: 15,
+    },
+
+    button: {
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#DF4723',
+        borderRadius: 4,
+        marginTop: 10,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16
+    },
+});
